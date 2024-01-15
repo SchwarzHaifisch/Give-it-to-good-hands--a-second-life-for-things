bba966504a7ffdf7c64c01f5eb7fa4af973eac8b
SchwarzHaifisch <franciszek.mateusz.wierzbicki@gmail.com>
2024-01-15 18:39:10 +0100

    Update

--- src/main/java/pl/coderslab/donation/DonationRepository.java
+++ src/main/java/pl/coderslab/donation/DonationRepository.java
@@ -9,7 +9,4 @@ public interface DonationRepository extends JpaRepository<Donation,Long> {
     @Query("SELECT SUM(d.quantity) FROM Donation d")
     Integer getAllDonations();
 
-    @Query("SELECT d FROM Donation d ORDER BY d.id DESC")
-    List<Donation> getInstitutions();
-
 }
